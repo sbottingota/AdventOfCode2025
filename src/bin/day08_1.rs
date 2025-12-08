c2510e69c0ccd671db5d4cbc5e34cb6ac1a4c811
sbottingota <saku.bottingota@gmail.com>
2025-12-08 18:46:16 +0000

    Day 8 part 1 (finally fixed woooooooo)

--- src/bin/day08_1.rs
+++ src/bin/day08_1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const INPUT_FILE: &str = "day08.txt";
 
 const N_CONNECTIONS: usize = 1000;
@@ -11,8 +13,81 @@ fn dist_square((x1, y1, z1): (u64, u64, u64), (x2, y2, z2): (u64, u64, u64)) -> 
     dx*dx + dy*dy + dz*dz
 }
 
+fn pairs_from_points(points: &[(u64, u64, u64)]) -> Vec<[(u64, u64, u64); 2]> {
+    let mut ret = Vec::new();
+
+    for (i, point1) in points.iter().enumerate() {
+        ret.append(&mut points.iter().skip(i+1).map(|point2| [*point1, *point2]).collect());
+    }
+
+    ret
+}
+
+fn adjacency_list_from_pairs(pairs: &[[(u64, u64, u64); 2]]) -> HashMap<(u64, u64, u64), Vec<(u64, u64, u64)>> {
+    let mut adjacency_list: HashMap<(u64, u64, u64), Vec<(u64, u64, u64)>> = HashMap::new();
+
+    for [point1, point2] in pairs {
+        if let Some(list) = adjacency_list.get_mut(&point1) {
+            list.push(point2.clone());
+        } else {
+            adjacency_list.insert(point1.clone(), vec![point2.clone()]);
+        }
+
+        if let Some(list) = adjacency_list.get_mut(&point2) {
+            list.push(point1.clone());
+        } else {
+            adjacency_list.insert(point2.clone(), vec![point1.clone()]);
+        }
+    }
+
+    adjacency_list
+}
+
+fn groups_from_adjacency_list(adjacency_list: &HashMap<(u64, u64, u64), Vec<(u64, u64, u64)>>) -> Vec<Vec<(u64, u64, u64)>> {
+    let mut groups: Vec<Vec<(u64, u64, u64)>> = Vec::new();
+
+    for point in adjacency_list.keys() {
+        let mut group = adjacency_list[point].clone();
+        group.push(*point);
+        groups.push(group);
+    }
+
+    let mut merged = true;
+    'outer: while merged {
+
+        merged = false;
+
+        for (i, group1) in groups.iter().enumerate() {
+            for group2 in &groups[i+1..] {
+                if group1.iter().any(|point| group2.contains(point)) {
+                    let merged_group: Vec<_> = group1
+                        .iter()
+                        .cloned()
+                        .chain(group2.into_iter().filter(|point| !group1.contains(&point)).cloned())
+                        .collect();
+
+                    let new_groups: Vec<_> = groups
+                        .iter()
+                        .filter(|group| *group != group1 && *group != group2)
+                        .cloned()
+                        .chain(std::iter::once(merged_group))
+                        .collect();
+
+                    groups = new_groups;
+
+                    merged = true;
+
+                    continue 'outer;
+                }
+            }
+        }
+    }
+
+    groups
+}
+
 fn main() {
-    let mut jboxes: Vec<(u64, u64, u64)> = Vec::new();
+    let mut points: Vec<(u64, u64, u64)> = Vec::new();
 
     for line in std::fs::read_to_string(INPUT_FILE)
         .expect("Error reading input file")
@@ -22,56 +97,21 @@ fn main() {
             .map(|substr| substr.parse().unwrap())
             .collect();
 
-        jboxes.push((values[0], values[1], values[2]));
+        points.push((values[0], values[1], values[2]));
     }
 
-    let mut connections: Vec<Vec<(u64, u64, u64)>> = Vec::new();
+    let mut pairs = pairs_from_points(&points);
+    pairs.sort_by_key(|[point1, point2]| dist_square(*point1, *point2));
+    pairs.drain(N_CONNECTIONS..);
 
-    for _ in 0..N_CONNECTIONS {
-        let mut min_box1 = (0_u64, 0_u64, 0_u64);
-        let mut min_box2 = (u32::MAX as u64, 0_u64, 0_u64); // replace with a better dummy value if necessary
+    let adjacency_list = adjacency_list_from_pairs(&pairs);
+    let groups = groups_from_adjacency_list(&adjacency_list);
 
-        for jbox1 in &jboxes {
-            let jbox2 = jboxes
-                .iter()
-                .filter(|jbox2| jbox1 != *jbox2 && !connections.iter().any(|connection| connection.contains(jbox1) && connection.contains(jbox2)))
-                .min_by_key(|pos| dist_square(**pos, *jbox1))
-                .unwrap();
+    let mut group_lens: Vec<usize> = groups.into_iter().map(|group| group.len()).collect();
+    group_lens.sort_by(|x, y| y.cmp(&x)); // sort descending
 
-            if dist_square(*jbox1, *jbox2) < dist_square(min_box1, min_box2) {
-                min_box1 = *jbox1;
-                min_box2 = *jbox2;
-            }
-        }
+    // println!("{:?}", group_lens);
 
-        let mut connected = false;
-        for connection in connections.iter_mut() {
-            let contains_box1 = connection.contains(&min_box1);
-            let contains_box2 = connection.contains(&min_box2);
-
-            if contains_box1 && contains_box2 {
-                connected = true;
-                break;
-            } else if contains_box1 {
-                connection.push(min_box2);
-                connected = true;
-                break;
-            } else if contains_box2 {
-                connection.push(min_box1);
-                connected = true;
-                break;
-            }
-        }
-
-        if !connected {
-            connections.push(vec![min_box1, min_box2]);
-        }
-    }
-
-    connections.sort_by(|connection1, connection2| connection2.len().cmp(&connection1.len()));
-
-    println!("{:?}", connections.iter().map(|connection| connection.len()).collect::<Vec<_>>());
-    // println!("{:?}", connections.iter().take(3).collect::<Vec<_>>());
-    println!("{}", connections.into_iter().take(3).map(|connection| connection.len()).product::<usize>());
+    println!("{}", group_lens.into_iter().take(3).product::<usize>());
 }
 
