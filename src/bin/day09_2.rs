ddde6d6375254e820a5d784af50bf10d5093f263
sbottingota <saku.bottingota@gmail.com>
2025-12-09 18:05:23 +0000

    Day 9 part 2 (using external lib for polygon intersections)

--- src/bin/day09_2.rs
+++ src/bin/day09_2.rs
@@ -1,5 +1,5 @@
-use itertools::Itertools;
-use std::collections::HashSet;
+use geo::algorithm::contains::Contains;
+use geo::{Polygon, Rect, LineString, Coord};
 
 const INPUT_FILE: &str = "day09.txt";
 
@@ -7,81 +7,27 @@ fn area_between((x1, y1): (u64, u64), (x2, y2): (u64, u64)) -> u64 {
     (x1.abs_diff(x2) + 1) * (y1.abs_diff(y2) + 1)
 }
 
-fn is_valid_enclosure((x1, y1): (u64, u64), (x2, y2): (u64, u64), red_tiles: &[(u64, u64)], green_tiles: &HashSet<(u64, u64)>) -> bool {
-    // swap coords if necessary
-    let (x1, x2) = if x1 > x2 { (x2, x1) } else { (x1, x2) };
-    let (y1, y2) = if y1 > y2 { (y2, y1) } else { (y1, y2) };
-
-    for x in x1..=x2 {
-        for y in y1..=y2 {
-            if !green_tiles.contains(&(x, y)) && !red_tiles.contains(&(x, y)) {
-                return false;
-            }
-        }
-    }
-
-    true
-}
-
 // find the largest area with red tiles for 2 of its vertices which only contains red and green tiles
-fn largest_area(red_tiles: &[(u64, u64)], green_tiles: &HashSet<(u64, u64)>) -> u64 {
+fn largest_area(red_tiles: &[(u64, u64)]) -> u64 {
+    let polygon = Polygon::new(LineString::from(red_tiles.iter().map(|(x, y)| (*x as f64, *y as f64)).collect::<Vec<_>>()), vec![]);
+
     let mut max_enclosed_area = 0_u64;
 
     for &tile1 in red_tiles {
         for &tile2 in red_tiles {
+            let rect = Rect::new(
+                Coord { x: tile1.0 as f64, y: tile1.1 as f64 },
+                Coord { x: tile2.0 as f64, y: tile2.1 as f64 }
+            ).to_polygon();
+                
             let enclosed_area = area_between(tile1, tile2);
-            if enclosed_area > max_enclosed_area
-                && is_valid_enclosure(tile1, tile2, red_tiles, green_tiles) {
-
+            if enclosed_area > max_enclosed_area && polygon.contains(&rect) {
                 max_enclosed_area = enclosed_area;
             }
         }
     }
 
     max_enclosed_area
-}
-
-fn get_green_tiles(red_tiles: &[(u64, u64)]) -> HashSet<(u64, u64)> {
-    let mut green_tiles = HashSet::new();
-
-    for (&(x1, y1), &(x2, y2)) in red_tiles.iter().circular_tuple_windows() {
-        if x1 == x2 {
-            let (y1, y2) = if y1 > y2 { (y2, y1) } else { (y1, y2) }; // swap if they're the other way around
-            for y in y1+1..y2 {
-                green_tiles.insert((x1, y));
-            }
-        } else { // y1 == y2
-            let (x1, x2) = if x1 > x2 { (x2, x1) } else { (x1, x2) }; // swap if they're the other way around
-            for x in x1+1..x2 {
-                green_tiles.insert((x, y1));
-            }
-        }
-    }
-
-    let red_tiles: HashSet<(u64, u64)> = HashSet::from_iter(red_tiles.to_vec());
-    println!("step 1 done");
-
-    for (i, mut tile) in green_tiles.clone().into_iter().enumerate() {
-        if i % 5_000 == 0 {
-            println!("{}/{}", i, green_tiles.len());
-        }
-
-        let mut new_tiles = HashSet::new();
-        while tile.0 > 0 {
-            tile.0 -= 1;
-            if green_tiles.contains(&tile) || red_tiles.contains(&tile) {
-                green_tiles.extend(&new_tiles);
-                break;
-            }
-
-            new_tiles.insert(tile);
-        }
-
-        // if the edge of the grid is reached, do nothing
-    }
-
-
-    green_tiles
 }
 
 fn main() {
@@ -97,10 +43,6 @@ fn main() {
         red_tiles.push((numbers[0], numbers[1]));
    }
 
-    let green_tiles = get_green_tiles(&red_tiles);
-
-    println!("parse");
-
-    println!("{}", largest_area(&red_tiles, &green_tiles));
+    println!("{}", largest_area(&red_tiles));
 }
 
